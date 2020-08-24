f2c8778848d305cb1cf26155d17faa78e0987644
Ariestellar <ahdrei47@gmail.com>
2020-08-24 20:13:16 +0300

    ImplementPusher

---
+++ Assets/Scripts/GameLogiс/PlatformStopper.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformStopper : MonoBehaviour
+{
+    [Range(0, 1)] [SerializeField] private float _speedStopped;
+    [SerializeField] private bool _braking = true;
+    [SerializeField] private Pusher _pusher;
+
+    private Rigidbody _ridgidbodyCurrentCat;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Cat")
+        {
+            _ridgidbodyCurrentCat = other.gameObject.GetComponent<Rigidbody>();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (_ridgidbodyCurrentCat && _braking)
+        {
+            _ridgidbodyCurrentCat.velocity = Vector3.Lerp(_ridgidbodyCurrentCat.velocity, Vector3.zero, _speedStopped);
+            if (_ridgidbodyCurrentCat.velocity == Vector3.zero)
+            {
+                _braking = false;                
+                LaunchPushingMechanism();
+            }
+        }        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Cat")
+        {
+            _ridgidbodyCurrentCat = null;
+            _braking = true;
+        }
+    }
+
+    private void LaunchPushingMechanism()
+    {
+        //если по x больше 6 то стоп
+        _pusher.Launch();
+    }
+}
---
+++ Assets/Scripts/GameLogiс/Pusher.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum DirectionForce
+{
+    left,
+    right,
+    forward,
+    back
+}
+
+public class Pusher : MonoBehaviour
+{
+    [SerializeField] private int _forceLaunch;
+    [SerializeField] private bool _isLaunch;    
+    [SerializeField] private DirectionForce _directionMove;    
+    [SerializeField] private Vector3 _directionForce;    
+    [SerializeField] private bool _isGo;    
+    [Range(0,1)][SerializeField] private float _speedRevert;    
+    
+    private Rigidbody _rigidbody;    
+    private Transform _transform;    
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _transform = GetComponent<Transform>();
+
+        if (_directionMove == DirectionForce.right)
+        {
+            _directionForce = Vector3.right;//не фризить позицию по x
+        }
+        else if (_directionMove == DirectionForce.left)
+        {
+            _directionForce = Vector3.left;//не фризить позицию по x
+        }
+        else if (_directionMove == DirectionForce.back)
+        {
+            _directionForce = Vector3.back;//не фризить позицию по z
+        }
+        else if (_directionMove == DirectionForce.forward)
+        {
+            _directionForce = Vector3.forward;//не фризить позицию по z
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isGo)
+        {
+            Launch();
+            _isGo = false;
+        }
+
+        if (_isLaunch)
+        {            
+            if (transform.localPosition.x > 6)
+            {                
+                _rigidbody.velocity = Vector3.zero;
+                ReverseMovement();
+                _isLaunch = false;
+            }
+            
+        }
+    }
+
+    public void Launch()
+    {        
+        _rigidbody.AddForce(_directionForce * _forceLaunch);
+        _isLaunch = true;
+    }
+
+    private void ReverseMovement()
+    {
+        //transform.localPosition = Vector3.Lerp(transform.localPosition, Vector3.zero, _speedRevert);
+        transform.localPosition = Vector3.zero;
+    }
+}
